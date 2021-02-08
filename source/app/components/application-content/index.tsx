d4d9ce1854eda36ddbe668638ee0bd6c799ea6c1
Simon Robertson <hello@simon-robertson.io>
2021-02-08 18:04:01 +0000

    +

--- source/app/components/application-content/index.tsx
+++ source/app/components/application-content/index.tsx
@@ -5,9 +5,11 @@ import { Article } from "app/components/article"
 import { Container } from "app/components/container"
 
 const ApplicationContent: FunctionComponent = () => {
-    const [clickCount, setClickCount] = useState(0)
+    const [buttonClickCount, setButtonClickCount] = useState(0)
     const [elementWidth, setElementWidth] = useState(0)
     const [elementHeight, setElementHeight] = useState(0)
+    const [inputValue, setInputValue] = useState("")
+    const [inputUpdateCount, setInputUpdateCount] = useState(0)
     const elementReference = useRef<HTMLDivElement>()
 
     useEffect(() => {
@@ -15,10 +17,21 @@ const ApplicationContent: FunctionComponent = () => {
         setElementHeight(elementReference.current.offsetHeight)
     }, [])
 
-    const increaseClickCount = (event: MouseEvent): void => {
+    const increaseClickCount = (event: Event): void => {
         if (event.isTrusted) {
             event.preventDefault()
-            setClickCount((value) => value + 1)
+            setButtonClickCount(buttonClickCount + 1)
+        }
+    }
+
+    const handleInput = (event: Event): void => {
+        if (event.isTrusted) {
+            event.preventDefault()
+
+            const input = event.currentTarget as HTMLInputElement
+
+            setInputValue(input.value)
+            setInputUpdateCount(inputUpdateCount + 1)
         }
     }
 
@@ -26,12 +39,16 @@ const ApplicationContent: FunctionComponent = () => {
         <div class="application-content" ref={elementReference}>
             <Container>
                 <Article title="Simple state test">
-                    <p>Click count is <strong>{clickCount}</strong></p>
-                    <button onClick={increaseClickCount}>Increase click count</button>
+                    <p>Button click count is <strong>{buttonClickCount}</strong></p>
+                    <button onClick={increaseClickCount}>Click (or tap)</button>
                 </Article>
                 <Article title="Element reference test">
                     <p>Content element width is {elementWidth}</p>
                     <p>Content element height is {elementHeight}</p>
+                </Article>
+                <Article title="Controlled input test">
+                    <p>Input update count is <strong>{inputUpdateCount}</strong></p>
+                    <input type="text" value={inputValue} onInput={handleInput} />
                 </Article>
             </Container>
         </div>
